889787c921057f0b9583e677e6741ca429a41a66
Eniep Yrekcaz <peinearydevelopment@gmail.com>
2015-07-28 11:19:59 -0500

    Code Analysis With JSHint and JSCS - Part 2
    Added conditional logic to js analysis task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,36 @@
 var gulp = require('gulp');
 
+var args = require('yargs').argv;
+var gulpif = require('gulp-if');
+var gulpprint = require('gulp-print');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util');
 
 gulp.task('vet', function() {
+    log('Analyzing source with JSHint and JSCS.');
+
     return gulp
         .src([
             './src/**/*.js',
-			'./*.js'
+            './*.js'
         ])
+        .pipe(gulpif(args.verbose, gulpprint()))
         .pipe(jscs())
         .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe(jshint.reporter('fail'));
 });
+
+//////////////////////////////////////////////////////////////////
+function log(message) {
+    if (typeof(message) === 'object') {
+        for (var item in message) {
+            if (message.hasOwnProperty(item)) {
+                util.log(util.colors.green(message[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.green(message));
+    }
+}
