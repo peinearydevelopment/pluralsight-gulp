8b2fb8e9e6322ebe82c43d6daf5ca4e13e6e4d2d
Eniep Yrekcaz <peinearydevelopment@gmail.com>
2015-07-28 11:55:01 -0500

    Code Analysis With JSHint and JSCS module - Part 3
    Added lazy loading of gulp plugins and config file to house file
    locations needed for gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,18 @@
-var gulp = require('gulp');
-
+var $ = require('gulp-load-plugins')({lazy: true});
 var args = require('yargs').argv;
-var gulpif = require('gulp-if');
-var gulpprint = require('gulp-print');
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var util = require('gulp-util');
+var config = require('./gulp.config')();
+var gulp = require('gulp');
 
 gulp.task('vet', function() {
     log('Analyzing source with JSHint and JSCS.');
 
     return gulp
-        .src([
-            './src/**/*.js',
-            './*.js'
-        ])
-        .pipe(gulpif(args.verbose, gulpprint()))
-        .pipe(jscs())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
-        .pipe(jshint.reporter('fail'));
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
 
 //////////////////////////////////////////////////////////////////
@@ -27,10 +20,10 @@ function log(message) {
     if (typeof(message) === 'object') {
         for (var item in message) {
             if (message.hasOwnProperty(item)) {
-                util.log(util.colors.green(message[item]));
+                $.util.log($.util.colors.green(message[item]));
             }
         }
     } else {
-        util.log(util.colors.green(message));
+        $.util.log($.util.colors.green(message));
     }
 }
